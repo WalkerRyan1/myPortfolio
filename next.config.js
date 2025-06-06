fe31fb8bf687e9bed21e8d37c9f2b837df52ab29
RyanWalker-FS <ryanwalker490@gmail.com>
2025-06-06 11:14:19 -0400

    Configure for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export", // Enable static exports
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: "/portfoliodemo", // Replace with your repository name
+  assetPrefix: "/portfoliodemo/", // Replace with your repository name
 };
 
 module.exports = nextConfig;
